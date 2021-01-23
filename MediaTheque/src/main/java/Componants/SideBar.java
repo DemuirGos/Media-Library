597755203d9c1b5321f2acafa934a87e6ed3e424
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2021-01-23 18:44:04 +0100

    added an untested first draft of the sideBar
    need to layout the mini components 
    needs to be tested

--- MediaTheque/src/main/java/Componants/SideBar.java
+++ MediaTheque/src/main/java/Componants/SideBar.java
@@ -1,5 +1,111 @@
 package Componants;
 
-public class SideBar {
+import MediaElements.IMedia;
+import DataBaseBloat.DataBaseApi;
+
+import java.awt.event.*;
+import java.util.*;
+import java.util.stream.*;
+
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class SideBar extends JFrame {
+    // search area
+    private JTextField SearchBar;
+    private JCheckBox global;
+    // items area
+    private JList<String> ItemsBar;
+    private JButton next;
+    private JButton prev;
+
+    // local loaded data area
+    private ArrayList<IMedia> items;
+
+    // local page parameters
+    int page = 1;
+    boolean isGlobal = false;
+
+    public SideBar() {
+        this.setSize(275, 560);
+        InitializeComponants();
+        HookEvents();
+        Fill();
+    }
+
+    private void InitializeComponants(){
+        SearchBar = new JTextField();
+        SearchBar.setSize(270, 40);
+        global = new JCheckBox();
+        global.setSize(35, 35);
+        ItemsBar = new JList<String>();
+        ItemsBar.setSize(270, 505);
+        next = new JButton("Next");
+        prev = new JButton("Previous");
+        prev.setSize(50, 30);
+        next.setSize(50, 30);
+    }
+
+    private void HookEvents(){
+        next.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                page++;
+                Fill();
+            }
+        });
+        prev.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                page = Math.max(page - 1, 1);
+                Fill();
+            }
+        });
+        SearchBar.getDocument().addDocumentListener(new DocumentListener() {
+            public void changedUpdate(DocumentEvent e) {
+                Search();
+            }
+            public void removeUpdate(DocumentEvent e) {
+              if(e.getLength() == 0){
+                Fill();
+              }
+              else{
+                Search();
+              }
+            }
+            public void insertUpdate(DocumentEvent e) {
+                Search();
+            }
+          });
+          global.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                isGlobal = !isGlobal;
+            }
+        });
+    }
+
+    private void Fill(ArrayList<IMedia> items) {
+        this.ItemsBar.removeAll();
+        for (IMedia item : items) {
+            JTextField element = new JTextField(item.getName());
+            element.setEditable(false);
+            this.ItemsBar.add(element);
+        }
+    }
+
+    private void Fill() {
+        items = new ArrayList<>(DataBaseBloat.DataBaseApi.Take(page, 16));
+        Fill(items);
+    }
+
+    private void Search() {
+        String word = this.SearchBar.getText();
+        ArrayList<IMedia> foundItems;
+        if (this.isGlobal) {
+            foundItems = new ArrayList<IMedia>(DataBaseApi.AllElements().stream().filter(itm -> itm.getName().contains(word)).collect(Collectors.toList()));
+        } else {
+            foundItems = new ArrayList<IMedia>(this.items.stream().filter(itm -> itm.getName().contains(word)).collect(Collectors.toList()));
+        }
+        Fill(foundItems);
+    }
+    private static final long serialVersionUID = 1L;
     
 }
