a71cdc5fce5b574af3c4cb69a129789e4806b5fc
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2021-01-23 12:37:26 +0100

    added db bloat initial version

---
+++ MediaTheque/src/main/java/DataBaseBloat/DataBaseApi.java
@@ -0,0 +1,68 @@
+package DataBaseBloat;
+
+import java.sql.*;
+import java.util.*;
+import java.util.List;
+import MediaElements.*;
+
+public class DataBaseApi {
+    private String JdbcDriver = "com.mysql.jdbc.Driver";  
+    private String DbLocation = "jdbc:mysql://localhost/EMP";
+    private String User = "usernamse";
+    private String Password = "password";
+
+    private Connection Connect() throws ClassNotFoundException, SQLException {
+        Class.forName(JdbcDriver);
+        System.out.println("Connecting to database...");
+        return DriverManager.getConnection(DbLocation,User,Password);
+    }
+
+    private List<IMedia> runQuery(String Query){
+        try(Connection local = Connect()){
+            try(Statement query = local.createStatement()){
+                try(ResultSet set = query.executeQuery(Query)){
+                    List<IMedia> results = new LinkedList<IMedia>();
+                    while(set.next()){
+                        //Retrieve by column name
+                        String name  = set.getString("Name");
+                        MediaType type  =  switch (set.getString("Type")) {
+                            case "Image" -> MediaType.Image;
+                            case "Video" -> MediaType.Video;
+                            case "Text"  -> MediaType.Text;
+                            case "Audio" -> MediaType.Audio;
+                            default -> throw new IllegalStateException("Invalid type");
+                        };
+                        String data = set.getString("Data");
+                        results.add(new IMedia(name,type,data));
+                    }
+                    return results;
+                }
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public void insert(IMedia element){
+        runQuery("INSERT INTO Medias VALUES (" + element.toString() + ")");
+    } 
+
+    public void remove(IMedia element){
+        runQuery("DELETE FROM Medias WHERE "                  + 
+                    "Name = " + element.getName()             + 
+                    "Type = " +  element.getType().toString() +
+                    "Data = " + element.getData()             );
+    } 
+
+    public List<IMedia> Select(IMedia element){
+        return  runQuery("SELECT Name, Type, Data FROM Medias WHERE " + 
+                             "Name = " + element.getName()            + 
+                             "Type =" +  element.getType().toString() +
+                             "Data = " + element.getData()            );
+    } 
+
+    public List<IMedia> SelectAll(){
+        return  runQuery("SELECT * FROM Medias");
+    } 
+}
