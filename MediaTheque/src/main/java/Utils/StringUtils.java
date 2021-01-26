8894839f70b910dfe14a8d80264a7aef8d87924f
DemuirGos <59487204+DemuirGos@users.noreply.github.com>
2021-01-26 15:08:43 +0100

    fixed export data errors

--- MediaTheque/src/main/java/Utils/StringUtils.java
+++ MediaTheque/src/main/java/Utils/StringUtils.java
@@ -4,11 +4,11 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class StringUtils {
-    public static String encode(String str) {
-        return new String(Base64.getEncoder().encode(str.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8);
+    public static String encode(byte[] byteData) {
+        return new String(Base64.getEncoder().encode(byteData));
     }
 
-    public static String decode(String str) {
-        return new String(Base64.getDecoder().decode(str.getBytes(StandardCharsets.UTF_8)));
+    public static byte[] decode(String str) {
+        return Base64.getDecoder().decode(str.getBytes());
     }
 }
